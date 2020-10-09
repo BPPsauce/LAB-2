cae86b5b6471a03ea49fc2ec86a4c111be7b721c
bgill87 <64290485+bgill87@users.noreply.github.com>
2020-10-09 10:54:59 -0700

    Oct9,10:54am - Bobby

--- main.cpp
+++ main.cpp
@@ -6,16 +6,65 @@
 #include "calender.hpp"
 
 int main(){
-  cout<<"Welcome to the appointment booking system, you can book your appointment here\n";
-  cout<<"You can book your appointment from Monday to Friday, from 0:00AM to 24:00PM\n";
-  cout<<"We will not open our services on weekend or the holidays, please be aware of this.\n";
+
   Date User_data1;
+  Date bookedAppts;
   TimeRange user_input;
   int user_month, user_date = 0;
   int user_start_hr =0;
   int user_start_min =0;
   int user_end_hr =0;
   int user_end_min =0;
+  int primaryInput;
+  cout<<"Welcome to the appointment system\n";
+  cout<<"Press 1 to view all booked appointments \n";
+  cout<<"Press 2 to view all available appointment time slots that have not been booked \n";
+  cout<<"Press 3 to book an appointment\n";
+  cout<<"Press 4 to check the status of an an appointment time slot\n";
+  cout<<"Press 5 to delete a booked appointment time\n";
+  cout<<"We will not be open on weekend or the holidays, please be aware of this.\n";
+  cin >> primaryInput;
+
+switch (primaryInput)
+ {
+
+ case 1:
+ // display all booked appointments
+ 
+  /* cout<<"Appointment summary:\n"; //printing the summary of the appointment
+  User_data1.output(user_month,user_date);
+  user_input.output(user_start_hr,user_start_min,user_end_hr,user_end_min); */
+ break;
+
+ case 2: 
+ // display all AVAILABLE appointments
+
+ char user_choice;
+  while (1)
+  {
+  cout<<"Would you like to see some infomation about this date?\n"<<"Enter 'A' to see appointed and free slots, 'N' for nothing\n";
+  cin>>user_choice;
+  if(user_choice == 'A'||user_choice == 'N'){
+    break;
+  }
+  else
+  {
+    cout<< "Sorry, please enter the right letter for the options\n";
+  }
+  }
+    
+  if(user_choice == 'A'){
+    User_data1.printAppointedTimeSlots(user_month,user_date,user_start_hr,user_start_min,user_end_hr,user_end_min);
+    User_data1.printFreeTimeSlots(user_month,user_date,user_start_hr,user_start_min,user_end_hr,user_end_min);
+  }
+  else if(user_choice == 'N'){
+    cout<<"Processing...\n";
+  }
+  break;
+ break;
+
+ case 3: 
+  
 
   char user_choice_date;//loop for keep asking month if user enters invalid number
   while (1)
@@ -30,7 +79,7 @@ int main(){
   }
   else
   {
-    cout<<"Please checkt your infomation!\n";
+    cout<<"Please ensure your information is correct!\n";
     cout<<"------------------------------\n";
   }
 }
@@ -91,7 +140,17 @@ int main(){
   else if(user_choice == 'N'){
     cout<<"Processing...\n";
   }
-  
+  break;
+
+  case 4:
+
+  //Check status of appointment time slot
+  break;
+
+  case 5:
+  // delete a scheduled appointment
+  break;
+
   cout<<"Appointment summary:\n";//printing the summary of the appointment
   User_data1.output(user_month,user_date);
   user_input.output(user_start_hr,user_start_min,user_end_hr,user_end_min);
@@ -107,4 +166,9 @@ int main(){
     cout<<"Sorry, try again next time :(\n";
   }
   return 0;
+}
+
+
+
+
 }
