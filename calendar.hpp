4912f8fa005e6eb368558197a94c65ef671dd612
Joanjoejoe <joannali820@yahoo.com>
2020-10-12 18:28:56 -0700

    query function not fully woring yet

--- calendar.hpp
+++ calendar.hpp
@@ -8,8 +8,10 @@ class calendar{
     public:
     calendar();
     bool book(Date date, TimeRange time);
-    bool query(Date date, TimeRange time);
-    bool Delete(Date date, TimeRange time);
+    bool query(Date date, TimeRange time);//check if its booked/free
+    //void queryApp(Date date, TimeRange time);//the function that actually gives out the output 
+    bool Delete(Date date, TimeRange time);//check if its deleted
+    void DeleteApp(Date date, TimeRange time);//function that actually delete appointment
     void printFreeTimeSlots_days(Date date, TimeRange time);
     //print all free days and timeslots in the calendar that can be booked
     void printFreeTimeSolts_days(Date date, TimeRange time);//print all free days and timeslots in the calendar that can be booked
@@ -25,15 +27,11 @@ class calendar{
     int getday ();
     int getyear();
 
-    
     private:
-  
-     Date availableDates[91]; 
+    Date availableDates[91]; 
     Date date;
     TimeRange time;
     int cal_month;
     int cal_day;
     int cal_year;
-
-
 };
