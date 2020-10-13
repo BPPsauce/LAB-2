a5f4e25a57c6a4395448e33d1d15987c443d8be3
Joanjoejoe <joannali820@yahoo.com>
2020-10-12 18:28:43 -0700

    query function not fully woring yet

--- calendar.cpp
+++ calendar.cpp
@@ -132,8 +132,6 @@ bool calendar::book(Date date, TimeRange time){//test
     }
     int print_index = 0;
 
-
-
 	if(date.isValid(date.getMonth(), date.getDate()) == 0){
 		cout<<"Appointment cannot be booked! Wrong month/date is given!\n";
 		return false;
@@ -156,23 +154,59 @@ bool calendar::book(Date date, TimeRange time){//test
 	return true;
 }
 
-bool calendar::query(Date date, TimeRange time){
+bool calendar::query(Date date, TimeRange time){//not working yet
 	int month = date.getMonth();
 	int day = date.getDate();
 	int start_hr = date.getStartTime_hr();
 	int start_min = date.getStartTime_min();
 	int end_hr = date.getEndTime_hr();
 	int end_min = date.getEndTime_min();
-	cout<< "The appointed time slot on "<<month<<" "<<day<<"is\n";
+	cout<< "Slots on "<<month<<" "<<day<<"is\n";
 	for (int i = 1;i < 48; i++ ){//print everything when the appointed array is false
-            if (date.appointed[i] == false){
-               cout<<slot[i]<<"\n";
+            if (date.appointed[i]){//need sometime to tell if the day is booked or free
+               cout<<"all free!\n";//this is wrong, not working yet
+			   return false;
             }
+			else{
+				cout<<"Someone has booked at least 1 time slot, please go back to the main menu to check\n";
+				return true;
+			}
         }
-		return false; ///// DOESNT RETURN ANYTHING YET
 	}
 
 bool calendar::Delete(Date date, TimeRange time){
+	int month = date.getMonth();
+	int day = date.getDate();
+	int start_hr = date.getStartTime_hr();
+	int start_min = date.getStartTime_min();
+	int end_hr = date.getEndTime_hr();
+	int end_min = date.getEndTime_min();
+	int start_index = 0;
+    int end_index = 0;
+    if (date.getStartTime_min() != 30){//calculation for the slot index number
+        start_index = (date.getStartTime_min() * 2);
+    }
+    else if (date.getStartTime_min() == 30){
+        start_index = (date.getStartTime_min() * 2) + 1;
+    }
+    if (date.getEndTime_min() == 30){
+        end_index = (date.getEndTime_min() * 2) + 1;
+    }
+    else if (date.getEndTime_min() != 30){
+        end_index = date.getEndTime_min() * 2;
+    }
+    int print_index = 0;
+    for (print_index = start_index; print_index < end_index; print_index++){
+        if(date.appointed[print_index] = true){
+			cout<<"Something is Wrong! Failed deleting]\n";
+			return false;
+		}
+    }
+	cout<<"Appointment deleted!\n";
+	return true;
+}
+
+void calendar::DeleteApp(Date date, TimeRange time){
 	int month = date.getMonth();
 	int day = date.getDate();
 	int start_hr = date.getStartTime_hr();
@@ -199,8 +233,7 @@ bool calendar::Delete(Date date, TimeRange time){
         cout<<slot[print_index]<<"\n";
 		date.appointed[print_index] = false;
     }
-	cout<<"Appointment deleted!\n";
-	return true;
+	cout<<"Deleting...\n";
 }
 
 
@@ -210,14 +243,3 @@ void calendar::printFreeTimeSlots_days(Date date, TimeRange time){
 
 }
 
-/*const string slot[48] ={//timeslot print
- "00:00 - 00:30","00:30 - 01:00","01:00 - 01:30","01:30 - 02:00","02:00 - 02:30"
-,"02:30 - 03:00","03:00 - 03:30","03:30 - 04:00","04:00 - 04:30","04:30 - 05:00"
-,"05:00 - 05:30","05:30 - 06:00","06:00 - 06:30","06:30 - 07:00","07:00 - 07:30"
-,"07:30 - 08:00","08:00 - 08:30","08:30 - 09:00","09:00 - 09:30","09:30 - 10:00"
-,"10:00 - 10:30","10:30 - 11:00","11:00 - 11:30","11:30 - 12:00","12:00 - 12:30"
-,"12:30 - 13:00","13:00 - 13:30","13:30 - 14:00","14:00 - 14:30","14:30 - 15:00"
-,"15:00 - 15:30","15:30 - 16:00","16:00 - 16:30","16:30 - 17:00","17:00 - 17:30"
-,"17:30 - 18:00","18:00 - 18:30","18:30 - 19:00","19:00 - 19:30","19:30 - 20:00"
-,"20:00 - 20:30","20:30 - 21:00","21:00 - 21:30","21:30 - 22:00","22:00 - 22:30"
-,"22:30 - 23:00","23:00 - 23:30","23:30 - 24:00"}; */
